a102415347abdfff539e0ebceef982d7101949e6
Luis Marciano <catalaojr@gmail.com>
2020-06-23 20:17:40 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/example/demo/resources/ClienteResource.java
+++ src/main/java/com/example/demo/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.example.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,15 +12,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.domain.Cliente;
 import com.example.demo.dto.ClienteDTO;
+import com.example.demo.dto.ClienteNewDTO;
 import com.example.demo.services.ClienteService;
 
 	
@@ -38,6 +42,14 @@ public class ClienteResource {
 		
 	}
 	
+	//@RequestMapping(method=RequestMethod.POST)
+		@PostMapping
+		public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+			Cliente obj = service.fromDTO(objDto);
+			obj = service.insert(obj);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();
+		}
 
 	//@RequestMapping(value="/{id}", method = RequestMethod.PUT)
 	@PutMapping("/{id}")
